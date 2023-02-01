58c919597511261a6d336f4508f4b7c7df89fecb
Francisco <f.v.borgia@gmail.com>
2023-01-31 22:11:17 -0500

    Improve composability tests

--- test/compose.test.ts
+++ test/compose.test.ts
@@ -34,4 +34,34 @@ describe('Compose operator', () => {
     expect(xshield()).toHaveProperty(['url'], '/testing');
     expect(xshield()).toHaveProperty(['options', 'cache'], 'no-cache');
   });
+
+  it('Immutability and Composability work', async () => {
+    const api = x.compose(
+      x.url('https://catfact.ninja'),
+      x.headers({ Accept: 'application/json' }),
+      x.headers({ 'Content-Type': 'application/text' }),
+    );
+
+    const breedsApi = x.compose(
+      api,
+      x.url('/breeds'),
+      x.headers({ 'Content-Type': 'application/json' }),
+    );
+
+    const factsApi = x.compose(
+      api,
+      x.url('/facts'),
+      x.headers({ 'Accept-Language': 'en-US,en;q=0.5' }),
+    );
+
+    // breeds
+    expect(breedsApi()).toHaveProperty(['url'], 'https://catfact.ninja/breeds');
+    expect(breedsApi().headers.get('Accept')).toBe('application/json');
+    expect(breedsApi().headers.get('Content-Type')).toBe('application/json');
+    // fatcs
+    expect(factsApi()).toHaveProperty(['url'], 'https://catfact.ninja/facts');
+    expect(factsApi().headers.get('Accept')).toBe('application/json');
+    expect(factsApi().headers.get('Content-Type')).toBe('application/text');
+    expect(factsApi().headers.get('Accept-Language')).toBe('en-US,en;q=0.5');
+  });
 });
