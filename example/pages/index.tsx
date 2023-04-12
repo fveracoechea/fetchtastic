16ca9a8d5c1f902022eae6221269a29a185d0abe
Francisco Veracoechea <francisco.veracoechea@magnitglobal.com>
2023-04-12 11:52:41 -0400

    Update example fallback

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -2,9 +2,11 @@ import { Suspense, useEffect } from 'react';
 import { controller } from '../utils/postApi';
 import dynamic from 'next/dynamic';
 
+const fallback = <p>Loading Cat Facts...</p>;
+
 const LazyPosts = dynamic(() => import('../components/Posts'), {
   ssr: false,
-  loading: () => <p>Loading Cat Facts...</p>,
+  loading: () => fallback,
 });
 
 export default function Docs() {
@@ -13,7 +15,7 @@ export default function Docs() {
       <h1>Example</h1>
       <button onClick={() => controller.abort()}>Abort Fetch</button>
       <br />
-      <Suspense fallback={<p>Loading Cat Facts...</p>}>
+      <Suspense fallback={fallback}>
         <LazyPosts />
       </Suspense>
     </div>
