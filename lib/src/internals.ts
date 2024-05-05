c492c0f5867ffbe12185339d03bebc000fc36090
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-05-05 15:13:52 -0400

    Cleanup internals

--- lib/src/internals.ts
+++ lib/src/internals.ts
@@ -35,21 +35,17 @@ export function getResponseParser(parser: ResponseParser) {
   };
 }
 
-export function isJsonBody(body: unknown) {
+export function shouldStringify(body: unknown, headers: Headers): boolean {
+  const isContentTypeJson =
+    headers.get('Content-Type') === 'application/json' ||
+    headers.get('content-type') === 'application/json';
+
   return (
-    typeof body === 'object' &&
+    isContentTypeJson &&
     !(body instanceof Blob) &&
     !(body instanceof ArrayBuffer) &&
     !(body instanceof ReadableStream) &&
     !(body instanceof FormData) &&
     !(body instanceof URLSearchParams)
   );
-}
-
-export function shouldStringify(body: unknown, headers: Headers): boolean {
-  const isContentTypeJson =
-    headers.get('Content-Type') === 'application/json' ||
-    headers.get('content-type') === 'application/json';
-
-  return isContentTypeJson ? true : isJsonBody(body);
 }
